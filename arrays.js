c30d8fd204bbf01529d37e640739b4abd3a1dde1
funny-teacher-0837 <detinsley1s@gmail.com>
2020-03-26 19:52:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ function addElementToBeginningOfArray(arr, ele) {
   return [ele, ...arr]
 }
 
-function destructiveAddElementToBeginningOfArray(arr, ele) {
+function destructivelyAddElementToBeginningOfArray(arr, ele) {
   arr.unshift(ele)
   return arr
 }
