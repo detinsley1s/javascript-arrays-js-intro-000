7170cac59d137119e910db6f2f664c75afca6743
funny-teacher-0837 <detinsley1s@gmail.com>
2020-03-26 19:40:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(arr, ele) {
+  return [ele, ...arr]
+}
+
+function destructiveAddElementToBeginningOfArray(arr, ele) {
+  arr.unshift(ele)
+  return arr
+}
